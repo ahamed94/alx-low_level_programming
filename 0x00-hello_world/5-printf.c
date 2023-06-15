93bc7284df19ec0e539027b44852aa12b3eaf9b8
Amr Hamed <amr.hamed1994@gmail.com>
2023-06-15 14:46:04 -0700

    printf usage

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - Just prints this line
+ * Return: Returns 0 when complete
+ */
+int main(void)
+{
+        printf("with proper grammar, but the outcome is a piece of art,\n");
+        return (0);
+}
