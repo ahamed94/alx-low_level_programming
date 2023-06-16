30bd39e18db4f40f4f2515699e8ac72a4b99c069
Amr Hamed <amr.hamed1994@gmail.com>
2023-06-16 13:02:12 -0700

    Alphabet twice

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Has to start here
+ * Return: Returns 0 when successful
+ */
+int main(void)
+{
+        char c;
+
+        for (c = 'a'; c <= 'z'; c++)
+                putchar(c);
+
+	for (c = 'A'; c <= 'Z'; c++)
+                putchar(c);
+        putchar('\n');
+        return (0);
+}
