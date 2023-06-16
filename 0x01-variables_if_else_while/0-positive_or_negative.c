e68c4ce038f40b1823162c28bf0d595e5975fe58
Amr Hamed <amr.hamed1994@gmail.com>
2023-06-16 11:58:56 -0700

    Wow forgot to include stdio

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 
 /**
  * main - That's where program starts
@@ -13,11 +14,11 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	if (n == 0)
-		printf("%d is zero", &n);
+		printf("%d is zero", n);
 	else if (n > 0)
-		printf("%d is positive", &n);
+		printf("%d is positive", n);
 	else
-		printf("%d is negative", &n);
+		printf("%d is negative", n);
 	printf("\n");
 	return (0);
 }
