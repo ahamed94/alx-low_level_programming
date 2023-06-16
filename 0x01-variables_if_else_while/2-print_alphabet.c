e23b529fc317a467c5ffc2bc0d3faf25b522ed49
Amr Hamed <amr.hamed1994@gmail.com>
2023-06-16 12:20:45 -0700

    Print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Has to start here
+ * Return: Returns 0 when successful
+ */
+int main(void)
+{
+        for (char c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	return (0);
+}
