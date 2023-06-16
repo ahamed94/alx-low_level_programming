04a51f297577730f351ef4259ae86f24d8d16414
Amr Hamed <amr.hamed1994@gmail.com>
2023-06-16 13:17:21 -0700

    Prnt all except q n e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Has to start here
+ * Return: Returns 0 when successful
+ */
+int main(void)
+{
+	char c;
+
+        for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c == 'e' || c=='q')
+			c++;
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
