73e000be42e468c72ce8994b6abcd2e85dbb2cec
Amr Hamed <amr.hamed1994@gmail.com>
2023-06-20 08:12:44 -0700

    Ms. Betty

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,14 @@
 #include "main.h"
 
+/**
+ * print_alphabet - Prints alphabet a->z 
+ * Return: 0
+ */
 void print_alphabet(void)
 {
 	char a = 'a';
+
 	while (a <= 'z')
 		_putchar(a++);
 	_putchar('\n');
-	return;
 }
