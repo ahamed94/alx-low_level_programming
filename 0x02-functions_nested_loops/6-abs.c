9132412f6bd6957b5187b5403504eff3cb388714
Amr Hamed <amr.hamed1994@gmail.com>
2023-06-20 10:26:25 -0700

    Coding like a pro

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - Returns absolute value
+ * @n: Number to get abs value for
+ * Return: Abs value
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+		return (n * -1);
+	else
+		return n;
+}
