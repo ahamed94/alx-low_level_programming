2e54e3c3e93cd1c47dd91e73a042335c9042fb9a
Amr Hamed <amr.hamed1994@gmail.com>
2023-06-20 08:23:53 -0700

    Not use previous fn

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,7 +8,16 @@
 void print_alphabet_x10(void)
 {
 	int i = 0;
+	char c;
 
 	while (i < 10)
-		print_alphabet();
+	{
+		c = 'a';
+
+		while (c <= 'z')
+			_putchar(c);
+
+		_putchar('\n');
+		i++;
+	}
 }
