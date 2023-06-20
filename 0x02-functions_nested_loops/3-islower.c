9ab688b50641f257a5e8ff6516a214b070bfb897
Amr Hamed <amr.hamed1994@gmail.com>
2023-06-20 08:41:09 -0700

    Adjust spacing

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,11 +2,11 @@
 
 /**
  * _islower - Determines whether Capital
- * @c - Char ascii that we check
+ * @parameterc - Char ascii that we check
  * Return: 1 if lowercase, 0 if otherwise
  */
 
 int _islower(int c)
 {
-	return(c/97);
+	return (c / 97);
 }
