3d6a902a168dc8a0b6f1ecd9a1734b1e1eaf264d
Amr Hamed <amr.hamed1994@gmail.com>
2023-06-20 16:08:32 -0700

    Betty

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,9 +13,9 @@ void times_table(void)
 	{
 		for (b = 0; b < 10; b++)
 		{
-			if(a * b < 10)
+			if (a * b < 10)
 			{
-				if(b !=0)
+				if (b != 0)
 					_putchar(' ');
 				_putchar(a * b + '0');
 			}
@@ -26,7 +26,7 @@ void times_table(void)
 				_putchar(a * b % 10 + '0');
 			}
 
-			if(b < 9)
+			if (b < 9)
 			{
 				_putchar(',');
 				_putchar(' ');
