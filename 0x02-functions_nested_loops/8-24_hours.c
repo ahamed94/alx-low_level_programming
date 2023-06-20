260810894170e64a12f3a47e00628260dd37fd49
Amr Hamed <amr.hamed1994@gmail.com>
2023-06-20 15:41:08 -0700

    Final tweak?

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,18 +8,22 @@
 
 void jack_bauer(void)
 {
-	int  i, j1, j2;
+	int  i1, i2, j1, j2;
 
-	for (i = 0; i < 24; i++)
-		for (j1 = 0; j1 < 6; j1++)
-			for (j2 = 0; j2 < 10; j2++)
-			{
-				if (i < 10)
-					_putchar('0');
-				_putchar(i);
-				_putchar(':');
-				_putchar(j1 + '0');
-				_putchar(j2+ '0');
-				_putchar('\n');
-			}	
+	for (i1 = 0; i1 < 3; i1++)
+		for (i = 0; i < 10; i++)
+		{
+			if (i1 == 2 && i2 == 4)
+				break;
+			for (j1 = 0; j1 < 6; j1++)
+				for (j2 = 0; j2 < 10; j2++)
+				{
+					_putchar(i1 + '0');
+					_putchar(i2 + '0');
+					_putchar(':');
+					_putchar(j1 + '0');
+					_putchar(j2+ '0');
+					_putchar('\n');
+				}	
+		}
 }
