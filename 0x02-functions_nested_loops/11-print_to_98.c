9a0494a8779c298076dc1581ae79abc6297aa4d8
Amr Hamed <amr.hamed1994@gmail.com>
2023-06-20 16:43:08 -0700

    print to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _islower - Determines whether Capital
+ * @c: char to be checked
+ * Return: 1 if lowercase, 0 if otherwise
+ */
+
+void print_to_98(int n)
+{
+	printf("%d", n);
+	if(n < 98)
+		while(n < 98)
+		{
+			printf(", ");
+			printf("%d", ++n);
+		}
+	else
+		while (n > 98)
+		{
+			printf(", ");
+			printf("%d", --n);
+		}
+}
