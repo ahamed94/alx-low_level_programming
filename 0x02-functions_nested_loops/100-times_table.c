d91f8f97b65188d02b3908102e5cdee02ca2301b
Amr Hamed <amr.hamed1994@gmail.com>
2023-06-20 17:34:16 -0700

    Betty?

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,24 +11,25 @@ void print_times_table(int n)
 {
 	int i, j;
 
-	if (n <= 15 && n < 0)
-		for (i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
+	{
+		if (n > 15 || n < 0)
+			break;
+		for (j = 0; j < n; j++)
 		{
-			for (j = 0; j < 10; j++)
-			{
-				if(j != 0)
-					if (i * j < 1000)
+			if (j != 0)
+				if (i * j < 1000)
+				{
+					printf(" ");
+					if (i * j < 100)
 					{
 						printf(" ");
-						if (i * j < 100)
-						{
+						if (i * j < 10)
 							printf(" ");
-							if (i * j < 10)
-								printf(" ");
-						}
 					}
-				printf("%d", i*j);
-			}
-			printf("\n");
+				}
+			printf("%d", i*j);
 		}
+		printf("\n");
+	}
 }
