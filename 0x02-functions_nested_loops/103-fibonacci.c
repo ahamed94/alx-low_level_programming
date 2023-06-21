e86b538ebb246465cb9893b8601265df27f30845
Amr Hamed <amr.hamed1994@gmail.com>
2023-06-20 18:19:22 -0700

    Add fibo

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,13 +7,13 @@
 
 int main(void)
 {
-        long int s1 = 1, s2 = 2, tmp, sum = 0;
+	long int s1 = 1, s2 = 2, tmp, sum = 0;
 	int i;
 
 	for (i = 2; i < 100; i++)
 	{
-	       if (s1 + s1 < 4000000 && i%2 == 0)
-		       sum += s1 + s2;
+		if (s1 + s1 < 4000000 && i % 2 == 0)
+			sum += s1 + s2;
 	       tmp = s1 + s2;
 	       s1 = s2;
 	       s2 = tmp;
