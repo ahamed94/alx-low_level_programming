d43e30800595f27b5e478cfec1e6f538f86b9721
Amr Hamed <amr.hamed1994@gmail.com>
2023-06-20 18:10:47 -0700

    Add fibo

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Has to start here
+ * Return: Returns 0 when successful
+ */
+
+int main(void)
+{
+        long int s1 = 1, s2 = 2, tmp, sum = 0;
+        int i;
+
+        printf("1, 2, ");
+        for (i = 2; i < 50; i++)
+        {
+		if (s1 + s1 < 4000000 && i%2 == 0)
+			sum += s1 + s2
+                tmp = s1 + s2;
+                s1 = s2;
+                s2 = tmp;
+                printf("%ld", s2);
+                if (i < 49)
+                        printf(", ");
+        }
+        printf("\n");
+        return (0);
+}
