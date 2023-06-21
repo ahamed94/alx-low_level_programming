5d908a7cac1e9681d145c44e26463027790c6ed6
Amr Hamed <amr.hamed1994@gmail.com>
2023-06-20 17:38:36 -0700

    Betty?

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -18,16 +18,18 @@ void print_times_table(int n)
 		for (j = 0; j <= n; j++)
 		{
 			if (j != 0)
+			{
+				printf(",");
 				if (i * j < 1000)
 				{
 					printf(" ");
 					if (i * j < 100)
 					{
 						printf(" ");
-						if (i * j < 10)
-							printf(" ");
+						if (i * j < 10)	printf(" ");
 					}
 				}
+			}
 			printf("%d", i * j);
 		}
 		printf("\n");
