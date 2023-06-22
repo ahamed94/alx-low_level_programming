983a2593e5a653cbffef02ad2679af951f46655b
Amr Hamed <amr.hamed1994@gmail.com>
2023-06-22 13:12:51 -0700

    Multiuply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplies 2 numbers
+ * @a: first operand
+ * @b: second operand
+ * Return: Returns result
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
