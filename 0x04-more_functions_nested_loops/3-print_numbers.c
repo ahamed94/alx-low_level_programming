819b5acb3b1892d08424665e4a9ce72615d191dc
Amr Hamed <amr.hamed1994@gmail.com>
2023-06-22 13:36:59 -0700

    print 0~9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints from 0~9
+ * Return: Returns 1 if upper, else 0
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+
+	printf("\n");
+}
