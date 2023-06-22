c3f9277adb4251176f4de6490c26ba82fb5de4ff
Amr Hamed <amr.hamed1994@gmail.com>
2023-06-22 12:34:45 -0700

    Capital or not

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isupper - returns 1 if uppercase
+ * @c: Char given
+ * Return: Returns 1 if upper, else 0
+ */
+
+int _isupper(int c)
+{
+	return (c < 91 && c > 64);
+}
