0dda5381acc7ea7524cb4111353ca9ceb43143ba
Amr Hamed <amr.hamed1994@gmail.com>
2023-06-26 11:37:14 -0700

    copyyy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strcpy - Copies str
+ * @dest: pointer to des string
+ * @src: string to be copied
+ *
+ * Return: Pointer to destination
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+	return (dest);
+}
