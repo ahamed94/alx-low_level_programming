e7b0142cb6b27323c5a4e0895a9b69d098d1f2aa
Amr Hamed <amr.hamed1994@gmail.com>
2023-06-26 03:12:50 -0700

    Swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - change value to 98
+ * @a: pointer to int1
+ * @b: pointer to int2
+ *
+ * Return: Nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+	int *tmp = *a;
+	*a = *b;
+	*b = *tmp;
+}
