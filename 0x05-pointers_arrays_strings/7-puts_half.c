2ea9e8016f5a2ee1e951cfa161297829f9b82dbb
Amr Hamed <amr.hamed1994@gmail.com>
2023-06-26 09:48:56 -0700

    print 2nd half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts_half - prints 2nd half
+ * @str: String
+ *
+ * Return: Nothing
+ */
+
+void puts_half(char *str)
+{
+	int size = strlen(str) - 1, i;
+
+	i = (size + size % 2) / 2;
+
+	while (i < size)
+		printf("%c", *(str + i++);
+
+	printf("\n");
+}
