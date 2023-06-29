0acfd8d73e70d6c8a87c1d0252fc43ada419c991
Amr Hamed <amr.hamed1994@gmail.com>
2023-06-29 06:38:23 -0700

    Compare str

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strcmp - Compares 2 strings
+ * @s1: First string to use to compare
+ * @s2: 2nd string to look in
+ *
+ * Return: 0 if match, less If less and more if more
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
