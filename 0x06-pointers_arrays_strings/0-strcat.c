c4155cd0593b74c9c79854636dfcc45a01903bbb
Amr Hamed <amr.hamed1994@gmail.com>
2023-06-29 06:19:49 -0700

    Concatenate

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strcat - Concatinates two strings
+ * @dest: first and source str
+ * @src: to be added
+ *
+ * Return: ptr to destination
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
