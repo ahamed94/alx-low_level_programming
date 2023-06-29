38be39f19cf489ec2f92d5fac8afb5485071ed42
Amr Hamed <amr.hamed1994@gmail.com>
2023-06-29 06:30:17 -0700

    Copies n bytes

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strcat - Copies one string to another 
+ * @dest: first and source str
+ * @src: to be copied
+ * @n: number of bytes
+ *
+ * Return: ptr to destination
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	return (strncpy(dest, src, n));
+}
