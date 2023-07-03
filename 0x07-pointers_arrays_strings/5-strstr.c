140e8cad7546553184a5f540d53e0e00b9f509fe
Amr Hamed <amr.hamed1994@gmail.com>
2023-07-03 03:14:30 -0700

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strstr - find str in string
+ * @haystack: ptr to str to look in
+ * @needle: str to find
+ *
+ * Return: ptr to str
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	return (strstr(haystack, needle));
+}
