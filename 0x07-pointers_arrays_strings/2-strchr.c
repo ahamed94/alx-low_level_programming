6199041830de68e5c9fffb94c6fd8cba50acff77
Amr Hamed <amr.hamed1994@gmail.com>
2023-07-03 03:05:02 -0700

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strchr - find chr in string
+ * @s: ptr to where to look
+ * @c: char to find
+ *
+ * Return: ptr to where it starts
+ */
+
+char *_strchr(char *s, char c)
+{
+	return (strchr(s, c));
+}
