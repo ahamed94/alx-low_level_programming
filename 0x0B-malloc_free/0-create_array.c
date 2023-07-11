33c984630d93c994870e907a700f1dfe624ee080
Amr Hamed <amr.hamed1994@gmail.com>
2023-07-11 09:16:05 -0700

    Create array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * create_array - creates array
+ * @size: size of array
+ * @c: character to init array
+ *
+ * Return: ptr to array or null if fails
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *ptr;
+
+	if (size > 0)
+	{
+		ptr = malloc(sizeof(*ptr) * size);
+		ptr[0] = c;
+		return (ptr);
+	}
+
+	else
+		return (NULL);
+}
