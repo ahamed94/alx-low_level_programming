06db7d7c6d2718151d15e6a10e91a3e9975b15da
Amr Hamed <amr.hamed1994@gmail.com>
2023-07-11 10:22:24 -0700

    Str concat 3

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,30 +10,35 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	int size = 0, i = 0;
-	char *ptr = NULL;
+	char *concat;
+	int len1 = 0, len2 = 0, i = 0, j = 0;
 
 	if (s1 == NULL)
 		s1 = "";
-
 	if (s2 == NULL)
 		s2 = "";
-
 	while (*(s1 + i))
-		size++, i++;
-	i = 0;
+		len1++, i++;
+	while (*(s2 + j))
+		len2++, j++;
+	len2++; /* add null terminator to length */
 
-	while (*(s2 + i))
-		size++, i++;
-	size++;
+	concat = malloc(sizeof(char) * (len1 + len2)); /*alloc memory*/
 
-	*ptr = malloc(sizeof(char) * size);
-
-	if (ptr == NULL)
+	if (concat == NULL) /* validate memory */
 		return (NULL);
 
-	strcat(ptr, s1);
-	strcat(ptr, s2);
+	i = 0, j = 0;
+	while (i < len1) /* concatenate */
+	{
+		*(concat + i) = *(s1 + i);
+		i++;
+	}
+	while (j < len2)
+	{
+		*(concat + i) = *(s2 + j);
+		i++, j++;
+	}
 
-	return (ptr);
+	return (concat);
 }
