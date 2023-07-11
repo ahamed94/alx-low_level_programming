ab49b17d512f001bb0a43c532505106c1f945eef
Amr Hamed <amr.hamed1994@gmail.com>
2023-07-11 09:33:42 -0700

    strdup hehe

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strdup - similiar to strdup
+ * @str: str to copy
+ *
+ * Return: ptr to array or null if fails
+ */
+
+char *_strdup(char *str)
+{
+	return (strdup(str));
+}
